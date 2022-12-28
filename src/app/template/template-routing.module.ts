3713eb9a03fdf48e6adb8b58417763ddff109bae
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-12-28 20:21:32 +0100

    start section 16 and Creation of required modules

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
+    RouterModule.forChild(routes)
   ]
 })
-export class AppRoutingModule { }
+export class TemplateRoutingModule { }
