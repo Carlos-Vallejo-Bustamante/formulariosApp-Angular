9a6deb0f504faeb8b148e4a82c66c70a660187a2
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-12-29 19:48:27 +0100

    add show errors in reactive Basicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -16,12 +16,16 @@ export class BasicosComponent {
   // })
 
   miFormulario: FormGroup = this.formBuilder.group({
-    nombre: ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
-    precio: [0, [Validators.min(0), Validators.required]],
-    existencias: [0, [Validators.min(0), Validators.required]]
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.min(0), Validators.required]],
+    existencias: [, [Validators.min(0), Validators.required]]
   })
 
   constructor(private formBuilder: FormBuilder) { }
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls['nombre'].touched
+  }
 
 
 }
