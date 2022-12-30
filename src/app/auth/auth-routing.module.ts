7ed5fe5cc13e2c4ebbaba5a8db2f96a68a7e0803
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-12-30 13:32:20 +0100

    add auth modules and components, lazyLoad and start section 18

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'registro', component: RegistroComponent },
+      { path: '**', redirectTo: 'login' }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthRoutingModule { }
